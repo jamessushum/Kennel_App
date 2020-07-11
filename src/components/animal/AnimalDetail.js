93c19f76e95fcb746034a7411362dfc384466edf
James Su-Shum <sushumjames@gmail.com>
2020-07-11 13:29:21 -0500

    added delete functionality in detail view for animal and location

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -3,11 +3,13 @@ import AnimalManager from '../../modules/AnimalManager';
 import './AnimalDetail.css';
 
 // Function responsible for rendering the animal details.
-const AnimalDetail = ({animalId}) => {
+const AnimalDetail = ({animalId, ...props}) => {
   // Initializing state with object of 2 properties currently empty.
   const [animal, setAnimal] = useState({name: "", breed: ""})
+  // Initializing state with 'true' boolean to disable/enable Discharge button upon data loading.
+  const [isLoading, setIsloading] = useState(true)
 
-  // useEffect invokes get(id) method in API passing-in the animalId being passed-in as a prop from ApplicationViews Route. Then setting the state with the response from API. useEffect will be re-invoked when there are changes to the [animalId].
+  // useEffect invokes get(id) method in API passing-in the animalId being passed-in as a prop from ApplicationViews Route. Then setting the state with the response from API. useEffect will be re-invoked when there are changes to the [animalId]. Setting isLoading to 'false' to enable Discharge button.
   useEffect(() => {
     AnimalManager.get(animalId)
       .then(response => {
@@ -15,8 +17,16 @@ const AnimalDetail = ({animalId}) => {
           name: response.name,
           breed: response.breed
         })
+        setIsloading(false)
       })
   }, [animalId])
+
+  // Delete method re-setting setIsloading back to 'true', invoking delete method in API passing-in animalId of animal to be deleted, then using props.history.push to re-direct user back to animalList.
+  const handleDelete = () => {
+    setIsloading(true)
+    AnimalManager.delete(animalId)
+      .then(() => props.history.push("/animals"))
+  }
 
   // Return the JSX of the animal card to be displayed in animal details.
   return (
@@ -27,6 +37,9 @@ const AnimalDetail = ({animalId}) => {
         </picture>
         <h3>Name: <span style={{color: 'darkslategrey'}}>{animal.name}</span></h3>
         <p>Breed: {animal.breed}</p>
+        <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Discharge
+        </button>
       </div>
     </div>
   )
--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -2,28 +2,42 @@ import React, { useState, useEffect } from 'react';
 import LocationManager from '../../modules/LocationManager';
 import './LocationDetail.css';
 
-const LocationDetail = ({locationId}) => {
+const LocationDetail = ({locationId, ...props}) => {
   // Initialize state of the data to be displayed
   const [location, setLocation] = useState({
     name: "",
     address: ""
   })
+  // Initialize state to disable/enable elements once data is loaded such as the delete button
+  const [isLoaded, setIsloaded] = useState(true)
 
   // Invoke API method getting single location object and populate the state with the API response, executed as a side effect
   useEffect(() => {
     LocationManager.get(locationId)
-      .then(response => setLocation({
+      .then(response => {setLocation({
         name: response.name,
         address: response.address
-      }))
+      })
+        setIsloaded(false)
+    })
   }, [locationId])
+
+  // Delete method re-sets setIsloaded flag back to 'true' and invoked API method to delete single item, then re-directs user back to locationList
+  const handleDelete = () => {
+    setIsloaded(true)
+    LocationManager.delete(locationId)
+      .then(() => props.history.push('/locations'))
+  }
 
   // JSX that will be rendered in DOM using data from state
   return (
     <div className="card">
       <div className="card-content">
-      <h3>{location.name}</h3>
-      <p>{location.address}</p>
+        <h3>{location.name}</h3>
+        <p>{location.address}</p>
+        <button type="button" disabled={isLoaded} onClick={handleDelete}>
+          Close Location
+        </button>
       </div>
     </div>
   )
