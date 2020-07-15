8b67a8b79115ab58ef12415c4a06f4f112b21cf3
James Su-Shum <sushumjames@gmail.com>
2020-07-15 15:41:48 -0500

    modified navbar display options per user auth and refactored code to pass down appopriate props

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -19,15 +19,14 @@ import OwnerEditForm from './owner/OwnerEditForm';
 import EmployeeWithAnimals from './employee/EmployeeWithAnimals';
 
 // Function responsible for what gets rendered in the display area of the application according to the user's input in the nav bar. <Route /> from the react-router-dom library delineates what gets rendered according to the <Link />'s path. <React.Fragment /> or </> is used in lieu of any additional containers. <Route /> for AnimalDetail looks for a path with animalId which is set by the <Link /> in the Details button in AnimalCard, the render then returns the AnimalDetail passing-in the animalId found in props.match.params and props using spread operator to access the history property. Conditional added to animals, locations, employees and owners list to check if user is logged-in.
-const ApplicationViews = () => {
-  // Method to check if credentials are stored in session storage
-  const isAuthenticated = () => sessionStorage.getItem('credentials') !== null
-
+const ApplicationViews = ({hasUser, setUser}) => {
   return (
     <React.Fragment>
       <Route
         exact path="/login"
-        component={Login}
+        render={props => {
+          return <Login setUser={setUser} {...props} />
+        }}
       />
       <Route 
         exact path="/"
@@ -37,77 +36,63 @@ const ApplicationViews = () => {
       />
       <Route
         exact path="/animals"
-        render={props => isAuthenticated() ? <AnimalList {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <AnimalList {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/animals/:animalId(\d+)"
-        render={props => {
-          return <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props} />
-        }}
+        render={props => hasUser ? <AnimalDetail animalId={parseInt(props.match.params.animalId)} {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path ="/animals/new"
-        render={props => {
-          return <AnimalForm {...props} />
-        }}
+        render={props => hasUser ? <AnimalForm {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/animals/:animalId(\d+)/edit"
-        render={props => isAuthenticated() ? <AnimalEditForm {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <AnimalEditForm {...props} /> : <Redirect to="/login" />}
       />
       <Route 
         exact path="/locations"
-        render={props => isAuthenticated() ? <LocationList {...props} /> : <Redirect to="/login" />}
+        render={props => <LocationList {...props} />}
       />
       <Route 
         exact path="/locations/:locationId(\d+)"
-        render={props => {
-          return <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props} />
-        }}
+        render={props => hasUser ? <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/locations/new"
-        render={props => {
-          return <LocationForm {...props} />
-        }}
+        render={props => hasUser ? <LocationForm {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/locations/:locationId(\d+)/edit"
-        render={props => isAuthenticated() ? <LocationEditForm {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <LocationEditForm {...props} /> : <Redirect to="/login" />}
       />
       <Route 
         exact path="/employees"
-        render={props => isAuthenticated() ? <EmployeeList {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <EmployeeList {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/employees/new"
-        render={props => {
-          return <EmployeeForm {...props} />
-        }}
+        render={props => hasUser ? <EmployeeForm {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/employees/:employeeId(\d+)/edit"
-        render={props => isAuthenticated() ? <EmployeeEditForm {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <EmployeeEditForm {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/employees/:employeeId(\d+)/details"
-        render={props => {
-          return <EmployeeWithAnimals {...props} />
-        }}
+        render={props => hasUser ? <EmployeeWithAnimals {...props} /> : <Redirect to="/login" />}
       />
       <Route 
         exact path="/owners"
-        render={props => isAuthenticated() ? <OwnerList {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <OwnerList {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/owners/new"
-        render={props => {
-          return <OwnerForm {...props} />
-        }}
+        render={props => hasUser ? <OwnerForm {...props} /> : <Redirect to="/login" />}
       />
       <Route
         exact path="/owners/:ownerId(\d+)/edit"
-        render={props => isAuthenticated() ? <OwnerEditForm {...props} /> : <Redirect to="/login" />}
+        render={props => hasUser ? <OwnerEditForm {...props} /> : <Redirect to="/login" />}
       />
     </React.Fragment>
   )
--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './nav/NavBar';
 import ApplicationViews from './ApplicationViews';
 import './Kennel.css';
 
 // Function responsible for what gets rendered in the DOM, child components get passed-in.
 const Kennel = () => {
+  // Method to check if credentials are stored in session storage
+  const isAuthenticated = () => sessionStorage.getItem('credentials') !== null
+
+  // Initializing user state with isAuthenticated method checking if session storage contains user credentials
+  const [hasUser, setHasUser] = useState(isAuthenticated())
+
+  // Method accepts user credentials as parameter and sets credentials in session storage, sets the hasUser state checking if credentials exist in session storage
+  const setUser = user => {
+    sessionStorage.setItem("credentials", JSON.stringify(user))
+    setHasUser(isAuthenticated())
+  }
+
+  // Returning JSX of NavBar and ApplicationViews, passing user state and setUser method as props
   return (
     <>
-      <NavBar />
-      <ApplicationViews />
+      <NavBar hasUser={hasUser} />
+      <ApplicationViews hasUser={hasUser} setUser={setUser} />
     </>
   )
 }
